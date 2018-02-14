a664e526d602605b2b39d38a3864c2cf36c0eddd
dirkdeyne <dirkdeyne@gmail.com>
2018-02-14 22:51:15 +0100

    basic hello-world example

---
+++ zk-spring-boot-demos/zkboot-hello/src/main/java/be/enyed/zkboot/ZkBootDemo.java
@@ -0,0 +1,16 @@
+package be.enyed.zkboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class ZkBootDemo {
+  
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(ZkBootDemo.class, args);
+  }
+  
+}
