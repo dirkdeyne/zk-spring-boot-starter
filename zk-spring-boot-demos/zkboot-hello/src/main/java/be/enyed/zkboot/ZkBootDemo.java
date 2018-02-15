c8bf3fa00eb00150b1c011c03f76c4888dee7771
dirkdeyne <dirkdeyne@gmail.com>
2018-02-15 11:28:54 +0100

    remove unused import

--- zk-spring-boot-demos/zkboot-hello/src/main/java/be/enyed/zkboot/ZkBootDemo.java
+++ zk-spring-boot-demos/zkboot-hello/src/main/java/be/enyed/zkboot/ZkBootDemo.java
@@ -3,7 +3,6 @@ package be.enyed.zkboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
