317324f17a113a9d252c7a44c2fb518cbbd80181
dirkdeyne <dirkdeyne@gmail.com>
2018-02-24 20:05:47 +0100

    you don't not need to be an Controller :)

--- zk-spring-boot-demos/zkboot-interaction/src/main/java/be/enyed/zkboot/ZkBootDemo.java
+++ zk-spring-boot-demos/zkboot-interaction/src/main/java/be/enyed/zkboot/ZkBootDemo.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@Controller
 public class ZkBootDemo {
   
   public static void main(String[] args) throws Exception {
