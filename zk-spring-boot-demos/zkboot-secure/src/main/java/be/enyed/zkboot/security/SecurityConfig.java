448fda0675c012847cf009edb616394fa3ea1000
dirkdeyne <dirkdeyne@gmail.com>
2018-03-11 21:50:20 +0100

    interaction zul-security through security-access-tld

--- zk-spring-boot-demos/zkboot-secure/src/main/java/be/enyed/zkboot/security/SecurityConfig.java
+++ zk-spring-boot-demos/zkboot-secure/src/main/java/be/enyed/zkboot/security/SecurityConfig.java
@@ -1,13 +1,12 @@
 package be.enyed.zkboot.security;
 
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   
   private static String currentUser = System.getProperty("user.name");
@@ -27,19 +26,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    
     // zk specific -- maybe disable CSRF set by default via auto-configuration? --
     http.csrf().disable() // can be disabled safely; ZK unique desktop ID generation prevents Cross-Site Request Forgery attacks
         
         // application specific
         .authorizeRequests()
+          .antMatchers("/zkau/web/**/js/**","/zkau/web/**/zul/css/**","/zkau/web/**/img/**").permitAll()
+          
+          .mvcMatchers("/","/login","/logout").permitAll()
           .mvcMatchers("/secure").hasRole("USER")
           .mvcMatchers("/admin").hasRole("ADMIN")
+          
           .antMatchers("/zkau/web/**/**.zul").denyAll() // calling a zul-page directly is not allowed -- should we put this in the auto-configuration to? --
-          .anyRequest().permitAll()
+          
         .and()
           .formLogin().loginPage("/login").defaultSuccessUrl("/")
         .and()  
-          .logout().clearAuthentication(true).logoutSuccessUrl("/");
+          .logout().logoutUrl("/logout").logoutSuccessUrl("/");
   }
   
 }
